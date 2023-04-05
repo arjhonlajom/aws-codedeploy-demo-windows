fe59ef18d49981cccb9a1004a20c53a63a2988af
AJ Lajom <arjhon.lajom@crayon.com>
2023-04-05 15:34:30 +0800

    updating startup script

--- scripts/start_server.ps1
+++ scripts/start_server.ps1
@@ -1,3 +1,5 @@
+#install vc_redist products
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://vcredist.com/install.ps1'))
 
 $url = "https://www.apachehaus.com/downloads/httpd-2.4.55-o111s-x86-vs17.zip" 
 $download_loc = "scripts/apache_.zip"
@@ -13,15 +15,15 @@ $TargetApacheFolder = "C:\Apache24"
 if (Test-Path $TargetApacheFolder) {
     Write-Host "Folder Exists. Dont do anything"
     # Remove-Item -Path $TargetApacheFolder -Confirm:$false -Force -Recurse
-    #Move-Item -Path $apache_extract_folder"/Apache24" -Destination $TargetApacheFolder -Force
+    # Move-Item -Path $apache_extract_folder"/Apache24" -Destination $TargetApacheFolder -Force
 }else{
     Write-Host "Folder Doesn't Exists"
     Move-Item -Path $apache_extract_folder"/Apache24" -Destination $TargetApacheFolder -Force
 }
 
 $apache_httpd_path = "C:\Apache24\bin\httpd.exe"
-Start-Process -NoNewWindow -FilePath $apache_httpd_path -ArgumentList "-k install"
-Start-Process -NoNewWindow -FilePath $apache_httpd_path -ArgumentList
+Start-Process -NoNewWindow -FilePath $apache_httpd_path -ArgumentList "-k install -n MyServiceName"
+Start-Process -NoNewWindow -FilePath $apache_httpd_path -ArgumentList "-k start -n MyServiceName"
 
 
 
